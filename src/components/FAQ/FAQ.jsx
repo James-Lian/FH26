37c1eb9b6c56038dc0c63144e0e5ef4548d569f6
James-Lian <166276178+James-Lian@users.noreply.github.com>
2026-03-05 03:41:31 -0500

    Update FAQ.jsx

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -21,7 +21,7 @@ const DEFAULT_FAQ = [
   },
   {
     q: "WILL THERE BE PRIZES?",
-    a: "Yes!! There will be prizes for the top 3 projects as well as for various categories that you can compete in! 🏆🎁",
+    a: "Yes!! There will be prizes for the top 3 projects as well as for various categories that you can compete in! 🏆",
   },
   {
     q: "WHAT DO I NEED TO BRING?",
@@ -29,7 +29,7 @@ const DEFAULT_FAQ = [
   },
   {
     q: "WILL HARDWARE BE PROVIDED?",
-    a: "For hardware hackers or teams looking to incorporate specialized hardware for their IoT/robotics/etc. projects, we encourage you to bring your own tools and equipment ⚙️🕹️! We may provide some basic components or kits, but we cannot guarantee provision of any specific tools. If YOU want to build with something specific, bring it! 🚀"
+    a: "For hardware hackers or teams looking to incorporate specialized hardware for their IoT/robotics/etc. projects, we encourage you to bring your own tools and equipment! We may provide some basic components or kits, but we cannot guarantee provision of any specific tools. If YOU want to build with something specific, bring it! 🚀"
   },
   {
     q: "ANY QUESTIONS WE DIDN'T ANSWER?",
